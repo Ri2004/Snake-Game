e36a18bce2a36a5fe8403e32defc12553010280d
Rishabh Chatterjee <rishabhchatterjee813@gmail.com>
2025-06-27 17:08:25 +0530

    Detect collision with food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -12,6 +13,7 @@ screen.title(titlestring="My Snake Game")
 # what is going on as output of the code, and after updating, again move turtles, and again call update() on screen object, and so on...
 screen.tracer(0)
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(key = "Up", fun = snake.up)
@@ -26,6 +28,8 @@ while game_is_on:
 
     snake.move()
 
-
+    # Detect Collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
